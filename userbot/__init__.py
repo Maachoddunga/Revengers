31c2c52fdd8c761a4b69ec55d31d1d14968307ab
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-02 21:50:34 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,3 +1,11 @@
-# Global Variables
+from userbot.Config import Var
 
+# Global Variables
 CMD_HELP = {}
+
+if Var.STRING_SESSION:
+    session_name = str(Var.STRING_SESSION)
+    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
