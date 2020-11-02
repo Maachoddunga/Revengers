5bfa96af61bacf3759dc1641cf195a58f27a1625
Maachoddunga <73837118+Maachoddunga@users.noreply.github.com>
2020-11-02 20:08:46 +0530

    Create Config.py

---
+++ userbot/Config.py
@@ -0,0 +1,14 @@
+import os
+
+class Var(object):
+    APP_ID = int(os.environ.get("APP_ID", 6))
+    # 6 is a placeholder
+    API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", None)
+    if PRIVATE_GROUP_ID is not None:
+        try:
+            PRIVATE_GROUP_ID = int(PRIVATE_GROUP_ID)
+        except ValueError:
+            raise ValueError(
+                "Invalid Private Group ID. Make sure your ID is starts with -100 and make sure that it is only numbers.")
