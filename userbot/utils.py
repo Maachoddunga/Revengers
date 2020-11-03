d163a1d2f38a23e425033c8097f8b91083a52f67
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-03 09:10:35 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -112,7 +112,6 @@ def load_module(shortname):
         mod.telebot = bot
         # auto-load
         mod.admin_cmd = admin_cmd
-        mod.sudo_cmd = sudo_cmd
         mod.edit_or_reply = edit_or_reply
         mod.eor = eor
         # support for paperplaneextended
