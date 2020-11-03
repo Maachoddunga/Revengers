40b54e2108dd8d11b72979187abf8a69ef71d955
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-03 09:56:23 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,6 +1,8 @@
-from userbot import name
+from userbot import bot
 
 @telebot.on(admin_cmd(pattern="alive"))
 async def lol(event):
+    itsme = bot.me
+    name = itsme.first_name
     await event.edit(f"Hi {name}, I'm alive and working!")
     
